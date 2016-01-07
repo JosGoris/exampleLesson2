80505cbd7ffc2c4b272b26bff4acb03404b77e87
JosGoris <jos.goris91@gmail.com>
2016-01-07 16:17:56 +0100

    added functions of lesson 2 and main.R to the project

---
+++ main.R
@@ -0,0 +1,18 @@
+#Jos Goris
+#Januari 2016
+#import packages
+library(raster)
+
+source('R/AgeCalculator.R')
+source('R/HelloWorld.R')
+source('R/MinusRaster.R')
+
+HelloWorld('Jos')
+AgeCalculator(1991)
+
+r <- raster(system.file("external/rlogo.grd", package="raster")) 
+r2 <- r 
+# Filling the rasterLayer with new values.
+r2[] <- (1:ncell(r2)) / 10
+# Performs the calculation
+r3 <- minusRaster(r, r2) 
